9a67785e70ad371ebda68b814d012fac4857fa2d
Devnish Mishra <devnishmishra@gmail.com>
2026-03-02 20:24:51 +0530

    Implemented add task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import Header from "./Components/Header";
 import ToDoList from "./Components/ToDoList";
 import "./index.css";
+import { useState } from "react";
 
 function App() {
-  const tasks = [
-    { id: 1, text: "Sample Task", completed: false }
-  ];
-
+  const[tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+  // const tasks = [
+  //   { id: 1, text: "Sample Task", completed: false }
+  // ];
+  function addTask(){
+    if(input.trim() === "") return; // Prevent adding empty tasks
+    const newTask={
+      id:Date.now(),
+      text:input,
+      completed:false
+    }
+    setTasks([...tasks,newTask]);
+    setInput(""); // Clear input after adding task
+  }
   return (
     <div className="container">
       <Header />
 
       <div className="input-section">
-        <input type="text" placeholder="Enter task..." />
-        <button>Add</button>
+        <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Enter task..." />
+        <button onClick={addTask}>Add</button>
       </div>
 
       <ToDoList tasks={tasks} />
